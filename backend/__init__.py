4dc93b6631ba422dba33f0c49dbd0f87942b0c69
Miguel Angulo <maangulo12@gmail.com>
2018-04-07 19:01:44 -0500

    Added Flask-Restless.

--- backend/__init__.py
+++ backend/__init__.py
@@ -9,10 +9,12 @@
     Flask extensions included in this application:
     - Flask-SQLAlchemy : Used for creating database models (using SQLAlchemy).
     - Flask-Migrate    : Used for performing SQLAlchemy database migrations.
+    - Flask-Restless   : Used for creating the REST API.
 """
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restless import APIManager
 from flask_migrate import Migrate
 
 
@@ -25,11 +27,15 @@ app = Flask(__name__,
 app.config.from_pyfile('config.py')
 
 # Initializing the Flask extensions
-db      = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db       = SQLAlchemy(app)
+restless = APIManager(app, flask_sqlalchemy_db=db)
+migrate  = Migrate(app, db)
 
 # Importing the database models
 from backend import models
+
+# Importing the API endpoints
+from backend import api
 
 # Importing the app views
 from backend import views
